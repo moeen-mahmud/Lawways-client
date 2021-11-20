64ea7a72379b0435b9c7196400c3f2c46d370191
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-20 14:32:00 +0600

    completed hero section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+      {/* <div className="mt-8 px-8"></div> */}
     </div>
   );
 };
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,30 @@
+// React
 import React from "react";
+
+// Hero image
+import HeroBG from "../../assets/hero-bg.jpg";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>This is hero component</h1>
-    </div>
+    <section className="flex flex-col items-center justify-between gap-4 mt-4 text-gray-100 bg-gray-800 md:flex-row">
+      <div className="md:w-1/2">
+        <img src={HeroBG} alt="Hero Background" />
+      </div>
+      <div className="px-4 md:w-1/2 ">
+        <h1 className="mb-6 font-serif text-4xl font-semibold leading-relaxed text-center md:leading-normal md:text-left">
+          A Law Firm At Your Palm
+        </h1>
+        <p className="leading-relaxed text-justify">
+          When you need legal help,{" "}
+          <span className="font-bold text-gray-200">Lawways</span> is here. Our
+          network of experienced lawyers can provide advice on legal issues for
+          you, your family, or your business, all directly through our app. With
+          no hourly fee or retainer, we help you worry less and live more. What
+          are you waiting for? Join now and speak with a lawyer in as little as
+          4 hours after initial contact.
+        </p>
+      </div>
+    </section>
   );
 };
 
