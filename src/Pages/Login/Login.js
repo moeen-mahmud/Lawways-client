394658f99edb864a62bd715aa338bbfaa4efa27f
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-22 17:04:35 +0600

    added login system

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FcGoogle } from "react-icons/fc";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 import BackButton from "../../components/BackButton/BackButton";
+import useAuth from "../../hooks/useAuth";
 
 const Login = () => {
+  const [userData, setUserData] = useState({});
+
+  const { logInUser, isLoading } = useAuth();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleUserData = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+
+    const newUserData = { ...userData };
+    newUserData[field] = value;
+    setUserData(newUserData);
+  };
+
+  const handleLogInUser = (e) => {
+    e.preventDefault();
+
+    logInUser(userData.email, userData.password, location, navigate);
+  };
+
   return (
     <>
       <BackButton />
@@ -13,40 +37,56 @@ const Login = () => {
           <h1 className="mb-4 font-serif text-4xl font-bold text-gray-800">
             Lawways
           </h1>
-          <p className="text-lg font-medium text-gray-500">
+          <p className="mb-4 text-lg font-medium text-gray-500">
             Log in or continue with Google
           </p>
-          <div className="my-4">
-            <form className="flex flex-col justify-center gap-4">
-              <input
-                className="p-2 text-gray-700 border border-gray-400 rounded"
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              <input
-                className="p-2 text-gray-700 border border-gray-400 rounded"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-              <button className="py-2 text-gray-200 bg-gray-800 rounded">
-                Log In
-              </button>
-            </form>
-          </div>
-          <p className="text-sm text-center">Or</p>
-          <div className="mt-4 text-center">
-            <button className="flex items-center justify-center w-full gap-4 py-2 border border-gray-700 rounded">
-              <FcGoogle /> Continue With Google
-            </button>
-            <h3 className="mt-4 text-sm font-medium text-gray-600">
-              Don't have an account?{" "}
-              <Link className="text-gray-800" to="/register">
-                Register here
-              </Link>
-            </h3>
-          </div>
+          {isLoading ? (
+            <ClipLoader color="#232832" loading={isLoading} size={60} />
+          ) : (
+            <>
+              <div className="mb-4">
+                <form
+                  onSubmit={handleLogInUser}
+                  className="flex flex-col justify-center gap-4"
+                >
+                  <input
+                    className="p-2 text-gray-700 border border-gray-400 rounded"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                    onBlur={handleUserData}
+                  />
+                  <input
+                    className="p-2 text-gray-700 border border-gray-400 rounded"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                    onBlur={handleUserData}
+                  />
+                  <button
+                    type="submit"
+                    className="py-2 text-gray-200 bg-gray-800 rounded"
+                  >
+                    Log In
+                  </button>
+                </form>
+              </div>
+              <p className="text-sm text-center">Or</p>
+              <div className="mt-4 text-center">
+                <button className="flex items-center justify-center w-full gap-4 py-2 border border-gray-700 rounded">
+                  <FcGoogle /> Continue With Google
+                </button>
+                <h3 className="mt-4 text-sm font-medium text-gray-600">
+                  Don't have an account?{" "}
+                  <Link className="text-gray-800" to="/register">
+                    Register here
+                  </Link>
+                </h3>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
