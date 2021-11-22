7f4dd13be2723db71ac1b4f7adca107087d58432
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-22 15:41:16 +0600

    refined login page and added registration page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import PlaceBooking from "./Pages/PlaceBooking/PlaceBooking";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/services/:id" element={<ServiceDetails />} />
           <Route path="/place-booking/:id" element={<PlaceBooking />} />
         </Routes>
