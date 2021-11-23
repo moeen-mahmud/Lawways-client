de02ed0b222b1bf6e0914797c8629082b2f8541e
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-23 18:48:18 +0600

    added admin pages

---
+++ src/Pages/Admin/ManageService/MangeService.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MangeService = () => {
+  return (
+    <div>
+      <h1>This is manage service</h1>
+    </div>
+  );
+};
+
+export default MangeService;
---
+++ src/Pages/Admin/ManageUserService/ManageUserService.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ManageUserService = () => {
+  return (
+    <div>
+      <h1>This is manage user services</h1>
+    </div>
+  );
+};
+
+export default ManageUserService;
