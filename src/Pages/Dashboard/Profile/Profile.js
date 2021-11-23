ad7717fd8f0af5c6611a90843d843930fd0c646d
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-23 13:04:40 +0600

    completed user profile

--- src/Pages/Dashboard/Profile/Profile.js
+++ src/Pages/Dashboard/Profile/Profile.js
@@ -4,8 +4,30 @@ import useAuth from "../../../hooks/useAuth";
 const Profile = () => {
   const { user } = useAuth();
   return (
-    <div>
-      <h1>This is user {user.displayName}</h1>
+    <div className="flex justify-center">
+      <section className="flex flex-col items-center justify-center gap-4">
+        <div>
+          {user.photoURL ? (
+            <img
+              style={{ borderRadius: "50%" }}
+              src={user.photoURL}
+              alt={user.displayName}
+            />
+          ) : (
+            <span
+              style={{ borderRadius: "50%" }}
+              className="px-5 py-4 text-2xl font-semibold text-gray-200 bg-gray-800"
+            >
+              {user.displayName?.slice(0, 1)}
+            </span>
+          )}
+        </div>
+        <div className="mt-4 text-center">
+          <h1 className="text-2xl font-semibold text-gray-800">
+            Welcome {user.displayName}
+          </h1>
+        </div>
+      </section>
     </div>
   );
 };
