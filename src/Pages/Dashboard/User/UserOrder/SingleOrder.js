0f706e3865a5bbdd0557d5112fa63e62391cc71e
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-23 13:31:57 +0600

    completed user order page

---
+++ src/Pages/Dashboard/User/UserOrder/SingleOrder.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const SingleOrder = ({ order }) => {
+  const { orderImage, orderItem, orderPrice, orderStatus } = order;
+  return (
+    <div className="flex items-center gap-4 px-2 py-4 rounded shadow-lg">
+      <div className="w-1/2">
+        <img
+          className="block mx-auto rounded-lg"
+          src={orderImage}
+          alt={orderItem}
+        />
+      </div>
+      <div className="w-full">
+        <h1 className="mb-2 text-lg font-semibold">{orderItem}</h1>
+        <p className="text-sm text-gray-700">
+          Charge: ${orderPrice === "145" ? orderPrice : `${orderPrice}/month`}
+        </p>
+        <div className="flex gap-2 mt-2">
+          <p
+            className={
+              orderStatus === "Pending"
+                ? "border-2 border-yellow-600 py-1 px-2 rounded text-sm text-yellow-600"
+                : "border-2 border-green-600 py-1 px-2 rounded text-sm text-green-600"
+            }
+          >
+            {orderStatus}
+          </p>
+          <button className="px-2 py-1 text-sm text-gray-100 bg-yellow-600 rounded active:bg-yellow-900">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleOrder;
--- src/Pages/Dashboard/User/UserOrder/UserOrder.js
+++ src/Pages/Dashboard/User/UserOrder/UserOrder.js
@@ -1,9 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import useAuth from "../../../../hooks/useAuth";
+import SingleOrder from "./SingleOrder";
 
 const UserOrder = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/orders?email=${user.email}`)
+      .then((res) => {
+        setOrders(res.data);
+      });
+  }, [user.email]);
+
   return (
     <div>
-      <h1>This is user order</h1>
+      <h1 className="mb-4 font-serif text-3xl font-semibold">My orders</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+        {orders.map((order) => (
+          <SingleOrder key={order._id} order={order} />
+        ))}
+      </div>
     </div>
   );
 };
