12da5d0e5efae1ef4ff337a31691cbb5b24054df
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-24 19:00:29 +0600

    added user services

--- src/Pages/Dashboard/Admin/ManageUserService/ManageUserService.js
+++ src/Pages/Dashboard/Admin/ManageUserService/ManageUserService.js
@@ -1,9 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import swal from "@sweetalert/with-react";
+
+import { IoMdTrash } from "react-icons/io";
+import { FcSynchronize } from "react-icons/fc";
+import { FaCheckCircle } from "react-icons/fa";
 
 const ManageUserService = () => {
+  const [userServices, setUserServices] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/orders").then((res) => {
+      setUserServices(res.data);
+    });
+  }, []);
+
+  const handleDelete = (id) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, then user won't be able to see his selected service",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        axios.delete(`http://localhost:5000/orders/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            swal("User order has been deleted successfully", {
+              icon: "success",
+            });
+            const newUserServices = userServices.filter(
+              (order) => order._id !== id
+            );
+            setUserServices(newUserServices);
+          }
+        });
+      }
+    });
+  };
+
+  const handleUpdate = (id) => {
+    swal({
+      title: "Want to update?",
+      text: "Status will be changed to approved",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willUpdate) => {
+      if (willUpdate) {
+        axios
+          .put(`http://localhost:5000/orders/${id}`, {
+            orderStatus: "Approved",
+          })
+          .then((res) => {
+            if (res.data.modifiedCount > 0) {
+              swal("Service status update successfully!", {
+                icon: "success",
+              });
+              window.location.reload();
+            } else {
+              swal("Service status already updated!", {
+                icon: "info",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div>
-      <h1>This is manage user services</h1>
+      <h1 className="font-serif text-3xl font-semibold text-gray-800">
+        Manage the users services
+      </h1>
+      <div className="hidden grid-cols-8 gap-4 p-4 pb-2 mt-6 mb-4 border-b-2 lg:grid place-items-center">
+        <h2 className="font-semibold text-gray-800">Name</h2>
+        <h2 className="font-semibold text-gray-800">Email</h2>
+        <h2 className="font-semibold text-gray-800">Address</h2>
+        <h2 className="font-semibold text-gray-800">Age</h2>
+        <h2 className="font-semibold text-gray-800">Phone</h2>
+        <h2 className="font-semibold text-gray-800">Service</h2>
+        <h2 className="font-semibold text-gray-800">Charge</h2>
+        <h2 className="font-semibold text-gray-800">Actions</h2>
+      </div>
+      {userServices.map((service) => (
+        <div
+          className="grid grid-cols-1 gap-4 p-4 my-4 text-center border rounded shadow-xl lg:border-b-2 lg:shadow-none lg:grid-cols-8 place-items-center"
+          key={service._id}
+        >
+          <p className="text-xs font-medium">{service.name}</p>
+          <p className="text-xs font-medium">{service.email}</p>
+          <p className="text-xs font-medium">{service.address}</p>
+          <p className="text-xs font-medium">{service.userAge}</p>
+          <p className="text-xs font-medium">{service.userPhone}</p>
+          <p className="text-xs font-medium">${service.orderItem}</p>
+          <p className="text-xs font-medium">${service.orderPrice}</p>
+          <div className="flex flex-col items-center justify-between gap-2">
+            <button
+              onClick={() => handleUpdate(service._id)}
+              className="flex items-center gap-3 px-4 py-2 transition-all border-2 rounded-full"
+            >
+              <span
+                className={`text-xs ${
+                  service.orderStatus === "Pending"
+                    ? "text-yellow-600"
+                    : "text-green-600"
+                }`}
+              >
+                {service.orderStatus}
+              </span>
+              {service.orderStatus === "Pending" ? (
+                <FcSynchronize />
+              ) : (
+                <FaCheckCircle className="text-green-600" />
+              )}
+            </button>
+            <button
+              onClick={() => handleDelete(service._id)}
+              className="flex items-center gap-3 px-4 py-2 text-red-500 transition-all border-2 rounded-full hover:text-red-700"
+            >
+              <span className="text-xs">Delete</span> <IoMdTrash />
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
