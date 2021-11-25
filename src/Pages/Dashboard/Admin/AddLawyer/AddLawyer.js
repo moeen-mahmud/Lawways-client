40147ebbdd8af8dc5fd19c66410a09c02a6b324d
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-25 15:30:30 +0600

    added lawyer with base64 image

--- src/Pages/Dashboard/Admin/AddLawyer/AddLawyer.js
+++ src/Pages/Dashboard/Admin/AddLawyer/AddLawyer.js
@@ -1,13 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import swal from "@sweetalert/with-react";
 import "../../../../App.css";
 const AddLawyer = () => {
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleAddLawyer = (e) => {
+    e.preventDefault();
+
+    if (!image) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("lawyerName", name);
+    formData.append("lawyerImage", image);
+    formData.append("lawyerDetails", details);
+
+    axios.post("http://localhost:5000/lawyers", formData).then((res) => {
+      if (res.data.insertedId) {
+        swal({
+          title: "Great!",
+          text: "The lawyer added successfully",
+          icon: "success",
+        }).then((onClose) => {
+          if (onClose) {
+            window.location.reload();
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div>
       <h1 className="font-serif text-3xl font-semibold text-gray-800">
         Add a lawyer
       </h1>
       <div className="mt-6 md:w-1/3">
-        <form>
+        <form onSubmit={handleAddLawyer}>
           <div className="flex flex-col gap-4 mb-4">
             <input
               className="p-2 text-gray-600 border rounded"
@@ -15,6 +48,7 @@ const AddLawyer = () => {
               name="name"
               placeholder="Lawyer Name"
               required
+              onBlur={(e) => setName(e.target.value)}
             />
             <textarea
               className="p-2 text-gray-600 border rounded"
@@ -22,15 +56,20 @@ const AddLawyer = () => {
               placeholder="Lawyer About"
               rows="5"
               required
+              onBlur={(e) => setDetails(e.target.value)}
             />
             <input
               className="text-gray-600 border rounded"
               type="file"
               name="image"
               accept="image/*"
+              onBlur={(e) => setImage(e.target.files[0])}
             />
           </div>
-          <button className="py-3 text-gray-200 bg-gray-800 rounded px-14 active:bg-gray-600">
+          <button
+            type="submit"
+            className="py-3 text-gray-200 bg-gray-800 rounded px-14 active:bg-gray-600"
+          >
             Add
           </button>
         </form>
