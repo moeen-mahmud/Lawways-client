3582ca489861421ba88e31b688933572646ea78a
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-25 19:22:08 +0600

    added payment method

---
+++ src/Pages/Dashboard/User/Payment/CheckoutForm.js
@@ -0,0 +1,110 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
+
+const CheckoutForm = ({ order }) => {
+  const { orderPrice, name, email } = order;
+  const price = parseInt(orderPrice);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [processing, setProcessing] = useState(false);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/create-payment-intent", { price })
+      .then((res) => setClientSecret(res.data.clientSecret));
+  }, [price]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    setProcessing(true);
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      setError("");
+      console.log(paymentMethod);
+    }
+
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: name,
+            email: email,
+          },
+        },
+      });
+
+    if (intentError) {
+      setError(intentError.message);
+      setSuccess("");
+    } else {
+      setError("");
+      setSuccess("Payment successful");
+      console.log(paymentIntent);
+      setProcessing(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="mb-4 text-2xl font-medium text-gray-600">
+        Pay with <span className="text-indigo-600">Stripe!</span>
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        {processing ? (
+          <ClipLoader color="#232832" loading={processing} size={20} />
+        ) : (
+          <button
+            className="px-8 py-2 mt-4 text-gray-100 bg-indigo-600 rounded "
+            type="submit"
+            disabled={!stripe}
+          >
+            Pay
+          </button>
+        )}
+      </form>
+      {error && <p className="mt-4 text-sm text-red-500">{error}</p>}
+      {success && <p className="mt-4 text-sm text-green-600">{success}</p>}
+    </div>
+  );
+};
+
+export default CheckoutForm;
--- src/Pages/Dashboard/User/Payment/Payment.js
+++ src/Pages/Dashboard/User/Payment/Payment.js
@@ -1,6 +1,13 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51JvvdnDU0PgBwZxqYuJcsCKEAUkKTTkLF0ZcQCXP1KEVLXmBvm6VjsZqwNCNyUujniFE7DiTAgClkWvn7cQmMmqO00IwWt9S1i"
+);
 
 const Payment = () => {
   const { orderId } = useParams();
@@ -17,26 +24,39 @@ const Payment = () => {
       <h1 className="font-serif text-3xl font-semibold text-gray-800">
         Billing Details
       </h1>
-      <div className="px-2 py-4 my-4 border rounded md:w-1/2">
-        <img
-          className="block w-1/3 mb-4 rounded"
-          src={order.orderImage}
-          alt={order.orderName}
-        />
-        <h2 className="my-2 text-xl font-semibold text-gray-600">
-          Selected Service
-        </h2>
-        <p className="font-medium text-gray-800 uppercase">{order.orderItem}</p>
-        <h2 className="my-2 text-xl font-semibold text-gray-600">
-          Service Charge
-        </h2>
-        <p className="font-medium text-gray-800 uppercase">
-          $
-          {order.orderPrice === "145"
-            ? order.orderPrice
-            : `${order.orderPrice}/month`}
-        </p>
+      <div className="flex gap-4 px-2 py-4 my-4 border rounded md:w-1/2">
+        <div className="md:w-1/3">
+          <img
+            className="block rounded"
+            src={order.orderImage}
+            alt={order.orderName}
+          />
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold text-gray-800">
+            Selected Service
+          </h2>
+          <p className="font-medium text-gray-600 uppercase">
+            {order.orderItem}
+          </p>
+          <h2 className="my-2 text-xl font-semibold text-gray-800">
+            Service Charge
+          </h2>
+          <p className="font-medium text-gray-600 uppercase">
+            $
+            {order.orderPrice === "145"
+              ? order.orderPrice
+              : `${order.orderPrice}/month`}
+          </p>
+        </div>
       </div>
+      {order?.orderPrice && (
+        <div className="px-2 py-4 border rounded md:w-2/4">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm order={order} />
+          </Elements>
+        </div>
+      )}
     </div>
   );
 };
